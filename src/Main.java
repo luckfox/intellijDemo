a6e7e23265a1b45f8e6f785e08ae38214754052e
luckfox <luckfox0927@gmail.com>
2022-03-26 14:22:39 +0800

    update main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
     {
         //This is mark for print hello World line 1
         //This is mark for print hello World line 2
+        //This is mark for print hello World line 3
         System.out.println("Hello World!");
     }
 }
